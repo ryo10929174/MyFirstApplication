cb3eccdc8af6c689168b0c3fe3a9005c87f4d6f7
github ryo10929174 <ryo-abiko@x-power.co.jp>
2022-05-30 11:31:58 +0900

    20220530

---
+++ src/main/java/com/example/demo/MyFirtstApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirtstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirtstApplication.class, args);
+	}
+
+}
